c493d77048b2f8da8c4b19c72026f04d7037cb00
Markos Borges <markos.borges@hotmail.com>
2023-01-20 18:52:09 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Html3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Menu.Show();
+        }
+    }
+}
---
+++ Viewer.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Text.RegularExpressions;
+using Html3;
+
+namespace EditorHtml
+{
+    public class Viewer
+    {
+        public static void Show(string text)
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Modo de VISUALIZAÇÃO");
+            Console.WriteLine("-------------------");
+            Replace(text);
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Pressione uma tecla para voltar ao menu");
+            Console.ReadKey();
+            Menu.Show();
+
+
+        }
+
+        public static void Replace(string text)
+        {
+
+            var strong = new Regex(@"<\s*strong[^>]*>(.*?)<\s*/\s*strong>");
+            var words = text.Split(' ');
+
+            for (var i = 0; i < words.Length; i++)
+            {
+                if (strong.IsMatch(words[i]))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(
+                        words[i].Substring(
+                            words[i].IndexOf('>') + 1,
+                            ((words[i].LastIndexOf('<') - 1) - words[i].IndexOf('>'))
+                        )
+                        );
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Write(words[i]);
+                    Console.Write(" ");
+                }
+            }
+
+        }
+    }
+}
